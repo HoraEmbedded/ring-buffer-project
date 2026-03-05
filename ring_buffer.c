a4af77d4005713fbf55d2a6133297572434a65cf
Lady_Horacia <azonhoumonhoracia@gmail.com>
2026-03-05 16:20:42 +0000

    feat: define RingBuffer struct with head, tail and count

--- ring_buffer.c
+++ ring_buffer.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
+#define BUFFER_SIZE 10
 
+typedef struct {
+    int buffer[BUFFER_SIZE];
+    int head;
+    int tail;
+    int count;
+} RingBuffer;
+
+int main() {
     return 0;
 }
